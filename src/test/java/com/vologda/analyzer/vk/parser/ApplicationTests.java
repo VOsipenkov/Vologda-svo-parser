0a455a238643c5d3bc69ca35435badbe486d2844
VOsipenkov <jumpViaOcean@gmail.com>
2023-03-04 17:15:24 +0300

    init

---
+++ src/test/java/com/vologda/analyzer/vk/parser/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vologda.analyzer.vk.parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
